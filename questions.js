f96c32f23d0fd7962903d7f77f9bef84f497f776
Joshua Lo <josh.lol1995@gmail.com>
2020-01-01 13:16:36 -0800

    can now dynamically generate and delete buttons

---
+++ questions.js
@@ -0,0 +1,30 @@
+let questions = [
+    {
+        title: "Commonly used data types DO NOT include:", 
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ___.", 
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Strings are immutable in Javascript", 
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    // {
+    //     title: 
+    //     choices:
+    //     answer: 
+    // },
+    // {
+    //     title:  
+    //     choices: 
+    //     answer: 
+    // },
+
+console.log("testing from question.js")
+
+]
